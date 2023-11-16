7539f4387d280e28e0b1b3e4c0261fa91b0873a7
Sergey Bobrovich <sabobrovich@alfaleasing.ru>
2023-11-16 16:27:12 +0300

    postgres, docker, liquibase

---
+++ src/main/resources/db/changelog/changelogs/schema_01.sql
@@ -0,0 +1,11 @@
+--liquibase formatted sql
+--changeset sbobrovich:create-table-goods
+CREATE TABLE public.goods
+(
+    id                BIGSERIAL   NOT NULL PRIMARY KEY UNIQUE,
+    description       VARCHAR(50) NOT NULL UNIQUE,
+    price             DECIMAL     NOT NULL CHECK ( price >= 0 ),
+    quantity_in_stock INTEGER CHECK ( quantity_in_stock >= 0 ),
+    wholesale_goods   BOOLEAN     NOT NULL default false
+);
+
